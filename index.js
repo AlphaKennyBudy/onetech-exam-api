6eda5050cc7bc4bcf4f97449fb83797d1ac3d50b
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-24 01:22:41 +0600

    A lot of things happened[4]
    	modified:   .env
    	new file:   .gitignore
    	modified:   index.js
    	modified:   package-lock.json
    	modified:   package.json

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const app = express();
 
-mongoose.connect("mongodb://localhost/onetech-exam", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
